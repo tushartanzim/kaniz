fd0a512e027627203836bd1f0136d66123d9ae3c
Md Tanzimul Hasan <72404003+tushartanzim@users.noreply.github.com>
2026-03-28 03:52:58 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import {
   UserCircle2,
   Phone,
 } from "lucide-react";
-import { Card, CardContent } from "./components/ui/Card";
+import { Card, CardContent } from "./components/ui/card";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Textarea } from "./components/ui/textarea";
